7f6df3393590bc1ad261d2882f608aa8cd9b3c87
andrejj999 <andrej999j@gmail.com>
2026-04-08 18:40:55 +0200

    header.php

---
+++ tapalaga_projekt/includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Komplexný sprievodca Linux distribúciami - Ubuntu, Fedora, Arch Linux a ďalšie">
+    <meta name="keywords" content="linux, distribúcie, open source, ubuntu, fedora, arch linux">
+    <meta name="author" content="Andrej">
+    <title><?php echo isset($pageTitle) ? $pageTitle . ' - LinuxDistro' : 'LinuxDistro'; ?></title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+    <nav class="navbar">
+        <div class="nav-container">
+            <div class="nav-logo">
+                <img src="" alt="" class="logo">
+                <span class="logo-text">LinuxDistro</span>
+            </div>
+            <ul class="nav-menu">
+                <li class="nav-item"><a href="index.php" class="nav-link">Domov</a></li>
+                <li class="nav-item"><a href="distributions.php" class="nav-link">Distribúcie</a></li>
+                <li class="nav-item"><a href="comparison.php" class="nav-link">Porovnanie</a></li>
+                <li class="nav-item"><a href="contact.php" class="nav-link">Kontakt</a></li>
+            </ul>
+            <div class="hamburger">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+        </div>
+    </nav>
